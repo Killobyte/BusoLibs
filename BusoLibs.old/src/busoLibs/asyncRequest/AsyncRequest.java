69b099e1a49dcbba73cac8e1b2ff0715567914ad
Shawn Busolits <shawn.busolits@gmail.com>
2012-09-01 16:17:00 -0400

    Added support for GET and POST

--- BusoLibs.old/src/busoLibs/asyncRequest/AsyncRequest.java
+++ BusoLibs.old/src/busoLibs/asyncRequest/AsyncRequest.java
@@ -10,7 +10,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
@@ -30,20 +29,12 @@ import java.util.List;
  */
 public class AsyncRequest extends Thread {
 
-	public static enum REQUEST_TYPE {
-		GET, POST
-	};
-
 	/** URL to be called for information */
 	String url;
 	/** Parameters for the call */
 	List<NameValuePair> params;
-	/** Headers for the request */
-	List<NameValuePair> headers;
 	/** Callback to make when the request completes */
 	RequestCallback callback;
-	/** Type of request */
-	REQUEST_TYPE type;
 
 	/**
 	 * Initializes the class with the provided URL and callback
@@ -56,35 +47,16 @@ public class AsyncRequest extends Thread {
 	 *            Callback to make when the request returns
 	 */
 	public AsyncRequest(String url, List<NameValuePair> params,
-			RequestCallback callback, REQUEST_TYPE type) {
+			RequestCallback callback) {
 		this.url = url;
 		this.params = params;
-		this.headers = null;
 		this.callback = callback;
-	}
-
-	public AsyncRequest(String url, List<NameValuePair> params,
-			List<NameValuePair> headers, RequestCallback callback,
-			REQUEST_TYPE type) {
-		this.url = url;
-		this.params = params;
-		this.headers = headers;
-		this.callback = callback;
-		this.type = type;
-	}
-
-	public void run() {
-		if (type == REQUEST_TYPE.GET) {
-			GET();
-		} else {
-			POST();
-		}
 	}
 
 	/**
 	 * Makes the request and calls the callback upon completion
 	 */
-	public void POST() {
+	public void run() {
 		try {
 			// Taken from
 			// http://stackoverflow.com/questions/693997/how-to-set-httpresponse-timeout-for-android-in-java
@@ -103,9 +75,7 @@ public class AsyncRequest extends Thread {
 			HttpClient httpclient = new DefaultHttpClient(httpParams);
 			HttpPost httppost = new HttpPost(url);
 
-			if (params != null) {
-				httppost.setEntity(new UrlEncodedFormEntity(params));
-			}
+			httppost.setEntity(new UrlEncodedFormEntity(params));
 
 			HttpResponse response = httpclient.execute(httppost);
 
@@ -121,40 +91,4 @@ public class AsyncRequest extends Thread {
 		}
 	}
 
-	public void GET() {
-		// Taken from
-		// http://stackoverflow.com/questions/693997/how-to-set-httpresponse-timeout-for-android-in-java
-		HttpParams httpParams = new BasicHttpParams();
-		// Set the timeout in milliseconds until a connection is established.
-		// The default value is zero, that means the timeout is not used.
-		int timeoutConnection = 10000;
-		HttpConnectionParams
-				.setConnectionTimeout(httpParams, timeoutConnection);
-		// Set the default socket timeout (SO_TIMEOUT)
-		// in milliseconds which is the timeout for waiting for data.
-		int timeoutSocket = 10000;
-		HttpConnectionParams.setSoTimeout(httpParams, timeoutSocket);
-
-		// Taken from
-		// http://www.androidsnippets.com/executing-a-http-post-request-with-httpclient
-		HttpClient httpclient = new DefaultHttpClient(httpParams);
-		HttpGet httpget = new HttpGet(url);
-
-		if(headers != null) {
-			for(NameValuePair header: headers) {
-				httpget.setHeader(header.getName(), header.getValue());
-			}
-		}
-		
-		try {
-			
-			HttpResponse response = httpclient.execute(httpget);
-			//Taken from http://stackoverflow.com/questions/2845599/how-do-i-parse-json-from-a-java-httpresponse
-			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-			String result = reader.readLine();
-			callback.doOnResult(result);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 }
